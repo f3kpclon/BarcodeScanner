27a870baba7e429c218f63130196f9c0144ef76a
Félix <Felix.sotelo.q@outlook.es>
2023-08-23 11:34:52 -0400

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Felix Sotelo on 23-08-23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
